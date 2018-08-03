3b8e43f4d25aa30cedb5b6a8082fbb0d59b3fdab
Tommaso <p.tommy93@gmail.com>
2018-08-03 15:17:27 +0200

    Fix cards order inside cards.jpg

--- misc/cut-image/script.py
+++ misc/cut-image/script.py
@@ -12,20 +12,41 @@ NEW_COLS = 10
 src = Image.open("cards.jpg")
 dst = Image.new('RGB', (X * NEW_COLS, Y * ROWS), (255, 255, 255))
 
-for x in range(0, X * COLS + 1, X):
-	for y in range(0, Y * ROWS - Y + 1, Y):
-		if x < X * 6:
-			box = (x, y, x + X, y + Y)
-
-			region = src.crop(box)
-			dst.paste(region, box)
+# Remove 3, 8, 9, 10
+for x in range(0, X * COLS - X, X):
+	for y in range(0, Y * ROWS, Y):
+		if x < X:
+			# Copy until 3
+			src_box = dst_box = (x, y, x + X, y + Y)
+		elif x == X:
+			# Skip 3
+			continue
+		elif x < X * 6:
+			# Copy until 7 (included) shifted by 1
+			src_box = (x, y, x + X, y + Y)
+			dst_box = (x - 1 * X, y, x + X - 1 * X, y + Y)
 		elif x == X * 6 or x == X * 7 or x == X * 8:
+			# Skip 8, 9, 10
 			continue
 		else:
+			# Copy J, Q, K shifted by 4
 			src_box = (x, y, x + X, y + Y)
-			dst_box = (x - 3 * X, y, x + X - 3 * X, y + Y)
+			dst_box = (x - 4 * X, y, x + X - 4 * X, y + Y)
 
-			region = src.crop(src_box)
-			dst.paste(region, dst_box)
+		region = src.crop(src_box)
+		dst.paste(region, dst_box)
+
+# Add 3 and A
+for y in range(0, Y * ROWS, Y):
+	src_box = (X, y, 2 * X, y + Y)
+	dst_box = (X * NEW_COLS - 2 * X, y, X * NEW_COLS - 1 * X, y + Y)
+	region = src.crop(src_box)
+	dst.paste(region, dst_box)
+
+for y in range(0, Y * ROWS, Y):
+	src_box = (X * COLS - 1 * X, y, X * COLS, y + Y)
+	dst_box = (X * NEW_COLS - 1 * X, y, X * NEW_COLS, y + Y)
+	region = src.crop(src_box)
+	dst.paste(region, dst_box)
 
 dst.save("output.jpg")
